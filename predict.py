4a57b30fb35522193dd9d2fa6dc41e6331c627ef
Mayanand <72348797+yMayanand@users.noreply.github.com>
2022-02-06 23:07:08 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -10,13 +10,16 @@ args = parser.parse_args()
 model = get_model(n_letters, 64, 128, n_categories)
 model.load_state_dict(torch.load('model.pt'))
 
-seq = list(args.name)
-for i, j in enumerate(seq):
-    seq[i] = all_letters.find(j)
-seq_len = len(seq)
-inp = torch.tensor(seq).unsqueeze(0)
-model.eval()
-out = model.infer(inp)
-_, idx = torch.max(out, dim=1)
-print(f'prediction for {args.name} is {all_categories[idx.item()]}')
+#seq = list(args.name)
+def predict(name):
+    seq = list(name)
+    for i, j in enumerate(seq):
+        seq[i] = all_letters.find(j)
+    seq_len = len(seq)
+    inp = torch.tensor(seq).unsqueeze(0)
+    model.eval()
+    out = model.infer(inp)
+    _, idx = torch.max(out, dim=1)
+    return all_categories[idx.item()]
+#print(f'prediction for {args.name} is {all_categories[idx.item()]}')
 

61970a7ebecabe3323d3375ea451d5e9a31f1776
Mayanand <72348797+yMayanand@users.noreply.github.com>
2022-02-07 09:04:34 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -3,14 +3,11 @@ import argparse
 from model import get_model
 from data import *
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--name", default="hinton", help="enter the name to predict country of origin")
-args = parser.parse_args()
 
 model = get_model(n_letters, 64, 128, n_categories)
 model.load_state_dict(torch.load('model.pt'))
 
-#seq = list(args.name)
+
 def predict(name):
     seq = list(name)
     for i, j in enumerate(seq):
@@ -21,5 +18,11 @@ def predict(name):
     out = model.infer(inp)
     _, idx = torch.max(out, dim=1)
     return all_categories[idx.item()]
-#print(f'prediction for {args.name} is {all_categories[idx.item()]}')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--name", default="hinton", help="enter the name to predict country of origin")
+    args = parser.parse_args()
+    seq = list(args.name)
+    predict(seq)
+    print(f'prediction for {args.name} is {all_categories[idx.item()]}')
 

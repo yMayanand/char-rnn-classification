e74d40ba3434822cfd14b507cb90050c84957f93
thinkin-machine <you@example.com>
2022-02-07 20:42:41 +0530

    yiiii

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from model import get_model
 from data import *
 
 
-model = get_model(n_letters, 32, 40, n_categories)
+model = get_model(n_letters, 128, 256, n_categories)
 
 parser = argparse.ArgumentParser('arguments for training')
 

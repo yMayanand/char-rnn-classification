6fa9a1aa478b9e14a35be533dcf2ab5e587a52c8
thinkin-machine <you@example.com>
2022-02-07 12:11:11 +0530

    add one more fc in model

--- model.py
+++ model.py
@@ -11,8 +11,9 @@ class RNN(nn.Module):
         self.hidden_size = hidden_size
         self.emb = nn.Embedding(vocab_len + 1, emb_dim, padding_idx=vocab_len)
         self.lstm = nn.LSTM(emb_dim, hidden_size, 1, batch_first=True)
-        self.fc = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
+        self.fc1 = nn.Linear(hidden_size, 256)
+        self.fc2 = nn.Linear(256, output_size)
+        
 
     def forward(self, inp, seq_lens):
         bs = inp.shape[0]
@@ -25,8 +26,8 @@ class RNN(nn.Module):
         for i, j in enumerate(lengths):
             store_out.append(out_padded[i, j.item()-1])
         out = torch.stack(store_out)
-
-        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
         return out
     def infer(self, inp):
         bs = inp.shape[0]
@@ -34,7 +35,8 @@ class RNN(nn.Module):
         inp = self.emb(inp)
         out, (h, c) = self.lstm(inp, (hidden, hidden))
         out = out[:, -1]
-        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
         return out
         
 def get_model(vocab_len, emb_dim, n_hidden, n_categories):
